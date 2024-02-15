d38fc8906b830f11d96c257a4bf48b1a087fa761
Mark Keeble <mjkeeble@yahoo.de>
2024-02-15 21:38:22 +0100

    add test songs

---
+++ data/index.ts
@@ -0,0 +1,8 @@
+import { TSong } from '../types';
+import all_that_i_can_do_is_sing_the_blues from './all_that_i_can_do_is_sing_the_blues.json';
+import black_coffee from './black_coffee.json';
+import the_thrill_is_gone from './the_thrill_is_gone.json';
+
+export const ALL_THAT_I_CAN_DO_IS_SING_THE_BLUES: TSong = all_that_i_can_do_is_sing_the_blues;
+export const BLACK_COFFEE: TSong = black_coffee;
+export const THE_THRILL_IS_GONE: TSong = the_thrill_is_gone;
