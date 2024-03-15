45ffdc35793832a54f0e3a01ebc89d41e8809e7f
Mark Keeble <mjkeeble@yahoo.de>
2024-03-15 12:58:17 +0100

    Refactor App.css and add Setlist route

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,12 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
-  console.log('start'); //******************************************
-
   return (
     <div id="App" className="h-full text-bj-white">
       <Routes>
         <Route path="/" element={<Gigs />} />
         <Route path="song/:id" element={<Song />} />
-
+        <Route path="setlist/" element={<Setlist />} />
         <Route path="setlist/:id" element={<Setlist />} />
       </Routes>
     </div>
