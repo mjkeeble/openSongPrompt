7b6fc07db6fa39bca909cd765237e009385b42aa
Mark Keeble <mjkeeble@yahoo.de>
2024-07-01 17:49:41 +0200

    add input validations

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+const gemaRegex = '^\\d{1,8}-\\d{3}$'
+
 export const hasMatchingBrackets = (str: string): boolean => {
   let depth = 0;
   for (let i = 0; i < str.length; i++) {
@@ -19,3 +21,11 @@ export const displayDate = (date: string): string => {
     year: 'numeric',
   });
 };
+
+export const validateGemaWerknummer = (gemaWerknummer: string): boolean => {
+  return new RegExp(gemaRegex).test(gemaWerknummer);
+};
+
+export const validateDuration = (minutes: number, seconds: number): boolean => {
+  return minutes >= 0 && seconds >= 0 && seconds < 60;
+}
