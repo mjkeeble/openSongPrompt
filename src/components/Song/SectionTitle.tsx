4fe35952d259a23bdc8e6dbeb114eab529296e7c
Mark Keeble <mjkeeble@yahoo.de>
2024-08-14 15:32:53 +0200

    make page count bold

--- src/components/Song/SectionTitle.tsx
+++ src/components/Song/SectionTitle.tsx
@@ -8,8 +8,8 @@ type TProps = {
 
 const PageTitle:React.FC<TProps> = ({currentPage, pageHasChords, title, totalPages}) => {
   return (
-    <div className={`flex ${pageHasChords ? "flex-col flex-start" : "flex-row justify-between w-1/2"} text-bj-green-light text-left`}>
-      <p className="text-5xl font-semibold">{title}</p>
+    <div className={`flex ${pageHasChords ? "flex-col flex-start" : "flex-row justify-between w-1/2"} text-bj-green-light  font-semibold text-left`}>
+      <p className="text-5xl">{title}</p>
 
       <p className="text-3xl">
         {currentPage}/{totalPages}
