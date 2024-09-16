5b77e18a9cc5fef96e0379dbab7358511604e036
Mark Keeble <mjkeeble@yahoo.de>
2024-09-16 17:39:49 +0200

    add JSON-server to repertoire

--- src/components/Repertoire/Repertoire.tsx
+++ src/components/Repertoire/Repertoire.tsx
@@ -2,31 +2,47 @@ import { storeSetlist } from '@context/index';
 import { useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { NavIndicator, SongListButton } from '..';
-import songs from '../../../data/songs.json';
-import { footswitch } from '../../const';
 import { TSong } from '../../types';
+
+import { footswitch } from '../../const';
 
 const Repertoire = () => {
   const navigate = useNavigate();
   const buttonsRef = useRef<HTMLButtonElement[]>([]);
   const [isLoaded, setIsLoaded] = useState(false);
-
-  // TODO: at the moment setlist is stored in local storage and
-
-  const repertoireList: TSong[] = songs
-    .sort((a, b) => a.title.localeCompare(b.title));
-  const songIds: number[] = songs.map((song: TSong) => song.id);
+  const [repertoireList, setRepertoireList] = useState<TSong[]>([]);
 
   useEffect(() => {
-    storeSetlist([0]);
-    if (buttonsRef.current[0]) {
-      buttonsRef.current[0].focus();
-    }
+    console.log('Getting songs');
+    const fetchSongs = async (): Promise<TSong[] | null> => {
+      try {
+        const response: TSong[] = await (await fetch('http://localhost:3000/songs')).json();
+        return response;
+      } catch (error) {
+        console.error('Error fetching songs', error);
+        return null;
+      }
+    };
+
+    const getAndSetSongs = async () => {
+      const songList = await fetchSongs();
+      if (songList) {
+        setRepertoireList(songList.sort((a, b) => a.title.localeCompare(b.title)));
+      }
+    };
+    getAndSetSongs();
   }, []);
 
+  useEffect(() => {}, []);
   useEffect(() => {
     const timerId = setTimeout(() => {
       setIsLoaded(true);
+      console.log('Storing setlist');
+      storeSetlist([0]);
+      if (buttonsRef.current[0]) {
+        console.log('setting focus');
+        buttonsRef.current[0].focus();
+      }
     }, 1000);
 
     return () => {
@@ -35,8 +51,12 @@ const Repertoire = () => {
   }, []);
 
   const endOfListRef = useRef<HTMLDivElement | null>(null);
+  console.log('🚀 --------------------------------------------------------------🚀');
+  console.log('🚀 => handleKeyDown => buttonsRef.current:', buttonsRef.current);
+  console.log('🚀 --------------------------------------------------------------🚀');
 
   const handleKeyDown = (event: { key: string }) => {
+    console.log('Keypress detected', event.key);
     if (isLoaded) {
       // TODO: refactor with switch
       const currentIndex = buttonsRef.current.findIndex((button) => button === document.activeElement);
@@ -59,7 +79,6 @@ const Repertoire = () => {
           endOfListRef.current.scrollIntoView({ behavior: 'smooth' });
         }
       } else if (event.key === footswitch.leftLong) {
-
         if (document.exitFullscreen) {
           document.exitFullscreen();
         }
@@ -70,11 +89,11 @@ const Repertoire = () => {
   const handleSelectSong = (id: number) => {
     let storageUpdateDebounce: NodeJS.Timeout | null = null;
 
-    console.log('storing to setlist', id);
-    storeSetlist(['BREAK', id]);
+    console.log('storing to setlist', Number(id));
+    storeSetlist(['BREAK', Number(id)]);
     if (storageUpdateDebounce) clearTimeout(storageUpdateDebounce);
     storageUpdateDebounce = setTimeout(() => {
-      console.log('storing to setlist', id);
+      console.log('storing to setlist', Number(id));
 
       navigate(`/song/1`);
     }, 1000);
@@ -85,8 +104,8 @@ const Repertoire = () => {
       <div onKeyDown={handleKeyDown} tabIndex={0}>
         <h1 className="my-5 font-fredericka text-7xl text-bj-white">Repertoire</h1>
         <ul className="mb-20 mt-8">
-          {songIds.map((songId: number, index) => {
-            const song: TSong | undefined = songs.find((song) => song.id === songId);
+          {repertoireList.map((song: TSong, index) => {
+            // const song: TSong | undefined = songs.find((song) => song.id === songId);
             if (!song) return null;
 
             return (
